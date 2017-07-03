5208819ea4a5e3269e8b1f498818847cf942828a
김성준 <pallet027@gmail.com>
2017-07-03 10:53:42 +0900

    Polymer test.

--- bootstrap.js
+++ bootstrap.js
@@ -5,47 +5,50 @@
         console.log($(this).val());
     }
 
+    if('registerElement' in document) {
 
-    var proto = Object.create(HTMLInputElement.prototype);
-    proto.createdCallback = function() {
-        this.type = "text";
-        this.addEventListener("keydown", textOnKeyDown);
-    };
+        var proto = Object.create(HTMLInputElement.prototype);
+        proto.createdCallback = function() {
+            this.type = "text";
+            this.addEventListener("keydown", textOnKeyDown);
+        };
 
-    document.registerElement("lh-number", {prototype: proto,extends: 'input'});
+        document.registerElement("lh-number", {prototype: proto,extends: 'input'});
 
-    proto = Object.create(HTMLInputElement.prototype);
-    proto.createdCallback = function() {
-        this.type = "text";
-        this.addEventListener("keydown", textOnKeyDown);
-    };
+        proto = Object.create(HTMLInputElement.prototype);
+        proto.createdCallback = function() {
+            this.type = "text";
+            this.addEventListener("keydown", textOnKeyDown);
+        };
 
-    document.registerElement("lh-eng", {prototype: proto,extends: 'input'});
+        document.registerElement("lh-eng", {prototype: proto,extends: 'input'});
 
-    proto = Object.create(HTMLInputElement.prototype);
-    proto.createdCallback = function() {
-        this.type = "radio";
-        $(this).wrap('<label class="lh-radio-label"/>').after($(this).attr("label"));
+        proto = Object.create(HTMLInputElement.prototype);
+        proto.createdCallback = function() {
+            this.type = "radio";
+            $(this).wrap('<label class="lh-radio-label"/>').after($(this).attr("label"));
+        };
 
-    };
+        document.registerElement("lh-radio", {prototype: proto,extends: 'input'});
 
-    document.registerElement("lh-radio", {prototype: proto,extends: 'input'});
+        proto = Object.create(HTMLInputElement.prototype);
+        proto.createdCallback = function() {
+            this.type = "checkbox";
+            $(this).wrap('<label class="lh-checkbox-label"/>').after($(this).attr("label"));
+        };
 
-    proto = Object.create(HTMLInputElement.prototype);
-    proto.createdCallback = function() {
-        this.type = "checkbox";
-        $(this).wrap('<label class="lh-checkbox-label"/>').after($(this).attr("label"));
-    };
+        document.registerElement("lh-checkbox", {prototype: proto,extends: 'input'});
 
-    document.registerElement("lh-checkbox", {prototype: proto,extends: 'input'});
+        proto = Object.create(HTMLInputElement.prototype);
+        proto.createdCallback = function() {
+            this.type = "text";
+            //$(this).after('<button is="lh-button">🗓</button>');
+        };
 
-    proto = Object.create(HTMLInputElement.prototype);
-    proto.createdCallback = function() {
-        this.type = "text";
-        $(this).after('<button is="lh-button">🗓</button>');
-    };
-
-    document.registerElement("lh-date", {prototype: proto,extends: 'input'});
+        document.registerElement("lh-date", {prototype: proto,extends: 'input'});
+    } else {
+        $("input[id=lh-radio]")
+    }
 
     $(document).on("click",'tabs > tab',function(e){
         $(this).siblings("tab[selected]").removeAttr("selected");
